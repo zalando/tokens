8a0bd07affd4c2db90285e9866f4bf02b469c68d
Michael Duergner <michael.duergner@zalando.de>
2016-07-30 12:41:23 +0200

    Should add some first JavaDoc
    Not finalized yet - simply to get first feedback on documentation style. See #50 for further information / discussion.

--- src/main/java/org/zalando/stups/tokens/Tokens.java
+++ src/main/java/org/zalando/stups/tokens/Tokens.java
@@ -19,11 +19,30 @@ import static org.zalando.stups.tokens.util.Objects.notNull;
 
 import java.net.URI;
 
-
+/**
+ * Use e.g. {@link Tokens#createAccessTokensWithUri(URI)} to create an {@link AccessTokensBuilder}
+ * which can be used to define multiple access tokens via {@link AccessTokensBuilder#manageToken(Object)}
+ * that are managed and refreshed.
+ *
+ * @author jbellmann
+ * @author sarnowski
+ * @author duergner
+ */
 public final class Tokens {
     private Tokens() {
     }
 
+    /**
+     * Create a new {@link AccessTokensBuilder} which can be used to define multiple <i>tokenId</i>s
+     * which can be used to retrieved access tokens from the supplied <i>accessTokenUri</i> which
+     * may have e.g. different <i>scopes</i>.
+     *
+     * @param accessTokenUri  The <i>accessTokenUri</i> to be used by the {@link AccessTokens}
+     *                        implementation created by the returned {@link AccessTokensBuilder} to
+     *                        get and refresh access tokens for the various defined <i>tokenId</i>s
+     * @return An {@link AccessTokensBuilder} which may be used to configure multiple <i>tokenIds</i>s
+     * with various <i>scopes</i> that are managed and refreshed automatically.
+     */
     public static AccessTokensBuilder createAccessTokensWithUri(final URI accessTokenUri) {
         return new AccessTokensBuilder(notNull("accessTokenUri", accessTokenUri));
     }
