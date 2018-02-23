95a5ab25957298401c49cf33ac05c96ec6068fc8
Joerg Bellmann <joerg.bellmann@zalando.de>
2018-02-23 20:49:40 +0100

    #99 updated after review-comment

--- src/main/java/org/zalando/stups/tokens/FileSupplier.java
+++ src/main/java/org/zalando/stups/tokens/FileSupplier.java
@@ -62,7 +62,7 @@ public class FileSupplier {
                     return new File(DEFAULT_CREDENTIALS_DIR);
                 } else {
                     throw new IllegalStateException(
-                            String.format("environment variable %s not set and default '/meta/credentials' not exists or not a directory.", CREDENTIALS_DIR_PROP));
+                            String.format("environment variable %s not set and default '%s' not exists or not a directory.", CREDENTIALS_DIR_PROP, DEFAULT_CREDENTIALS_DIR));
                 }
             }
         }
